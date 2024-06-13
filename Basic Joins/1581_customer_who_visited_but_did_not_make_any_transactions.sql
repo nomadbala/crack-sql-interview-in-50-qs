b0ebf8e85474460f468d59cc503fc59f0f25838d
Sayat Sapar <devsayat01@gmail.com>
2024-06-13 21:52:03 +0500

    Create 1581_customer_who_visited_but_did_not_make_any_transactions.sql

---
+++ Basic Joins/1581_customer_who_visited_but_did_not_make_any_transactions.sql
@@ -0,0 +1,6 @@
+SELECT V.customer_id, COUNT(V.visit_id) AS count_no_trans
+FROM Visits AS V
+LEFT JOIN Transactions AS T
+ON V.visit_id = T.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY V.customer_id
